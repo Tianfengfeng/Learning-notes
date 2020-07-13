f6ec7611db840dba2294054019b44485b6f564dc
tianfeng <taloingsoft@163.com>
2020-07-13 08:55:44 +0800

    +

--- typeScript/index.ts
+++ typeScript/index.ts
@@ -1,9 +1,9 @@
 
 //  typescript  基础类型学习
 
-class Mytype2{
+class myType2{
     enum(){
-        //1. 用于定义数值集合  可以定义一些带名字的常量   git更新了
+        //1. 用于定义数值集合  可以定义一些带名字的常量   
         //2. 枚举类型的值是数值类型 因此又被称为数字类型枚举
         // 3. 成员的值可以不用初始化 ，因为他们有自增行为
         enum person{
@@ -44,23 +44,23 @@ class Mytype2{
 
     }
     myArr(){
-        let arr1 : Array<number>=[1,2,1,2,1,3,1,2,3,4,6,4,6,4,5,6]
-        let newarr:number[]=arr1.reduce((pre,cur,index,arr1)=>{
+        let arr1 : Array<number>=[1,2,1,2,1,3,1,2,3,4,6,4,6,4,5,6] 
+        let newarr:number[]=arr1.reduce((pre,cur,index,arr1)=>{  
             pre.indexOf(cur)===-1 && pre.push(cur)
             return pre
         },[])
-        console.log(newarr)
+        console.log(newarr) 
         let isDa: boolean=arr1.every(item=>{return item>3}) //判断每一项
         console.log(isDa)
         let isda2:boolean=arr1.some(item=>{ return item=6}) // 判断是否存在一项满足条件
 
 
 
-        // forEach()、map()、every()、some()和filter()   reduce
+        // forEach()、map()、every()、some()和filter()   reduce   
         // 只传一个匿名函数
         arr1.forEach(function(item,index,array){
             console.log(this);  // window
-        });
+        });  
         // 传两个参数
         arr1.forEach(function(item,index,array){
             console.log(this);  // [1, -2, 3, 4, -5]
